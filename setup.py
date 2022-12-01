623545121c009fb5882a19303521b2c3ee4fea14
Matthews <odos.matthews@afs.com>
2022-12-01 14:02:34 -0500

    add modules and functions

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="sqlalchemize",
-    version="0.0.1",
+    version="0.0.2",
     description="Make changes to sql tables using SqlAlchmey",
     long_description=README,
     long_description_content_type="text/markdown",
