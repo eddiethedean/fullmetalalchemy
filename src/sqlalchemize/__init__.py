c6c449c4f416eed48b575b592a76730a32701e17
Odos Matthews <odosmatthews@Odoss-Mac-mini.local>
2023-01-05 14:08:20 -0500

    add sql to python type converter

--- src/sqlalchemize/__init__.py
+++ src/sqlalchemize/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 
 import sqlalchemize.create as create
 import sqlalchemize.delete as delete
--- src/sqlalchemize/type_convert.py
+++ src/sqlalchemize/type_convert.py
@@ -24,6 +24,39 @@ _type_convert = {
     dict: sql.sqltypes.JSON
 }
 
+_sql_to_python = {
+    sql.sqltypes.Integer: int,
+    sql.sqltypes.SmallInteger: int,
+    sql.sqltypes.SMALLINT: int,
+    sql.sqltypes.BigInteger: int,
+    sql.sqltypes.BIGINT: int,
+    sql.sqltypes.INTEGER: int,
+    sql.sqltypes.Unicode: str,
+    sql.sqltypes.NVARCHAR: str,
+    sql.sqltypes.NCHAR: str,
+    sql.sqltypes.Float: decimal.Decimal,
+    sql.sqltypes.REAL: decimal.Decimal,
+    sql.sqltypes.FLOAT: decimal.Decimal,
+    sql.sqltypes.Numeric: decimal.Decimal,
+    sql.sqltypes.NUMERIC: decimal.Decimal,
+    sql.sqltypes.DECIMAL: decimal.Decimal,
+    sql.sqltypes.DateTime: datetime.datetime,
+    sql.sqltypes.TIMESTAMP: datetime.datetime,
+    sql.sqltypes.DATETIME: datetime.datetime,
+    sql.sqltypes.LargeBinary: bytes,
+    sql.sqltypes.BLOB: bytes,
+    sql.sqltypes.Boolean: bool,
+    sql.sqltypes.BOOLEAN: bool,
+    sql.sqltypes.MatchType: bool,
+    sql.sqltypes.Date: datetime.date,
+    sql.sqltypes.DATE: datetime.date,
+    sql.sqltypes.Time: datetime.time,
+    sql.sqltypes.TIME: datetime.time,
+    sql.sqltypes.Interval: datetime.timedelta,
+    sql.sqltypes.ARRAY: list,
+    sql.sqltypes.JSON: dict
+}
+
 
 def get_sql_types(data: Mapping[str, Sequence]) -> list:
     return [get_sql_type(values) for values in data.values()]
