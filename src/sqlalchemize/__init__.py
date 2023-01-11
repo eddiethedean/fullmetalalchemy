b55321e355b3d1e5d4e356c032ccf514ad7267b2
Odos Matthews <odosmatthews@Odoss-MacBook-Pro.local>
2023-01-11 15:08:32 -0500

    add create_table_from_records function

--- src/sqlalchemize/__init__.py
+++ src/sqlalchemize/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.6'
+__version__ = '0.1.0'
 
 import sqlalchemize.create as create
 import sqlalchemize.delete as delete
--- src/sqlalchemize/create.py
+++ src/sqlalchemize/create.py
@@ -1,11 +1,18 @@
-from typing import Optional, Sequence
+from typing import Any, Iterable, Optional, Sequence
+import decimal
+import datetime
 
 import sqlalchemy as sa
 import sqlalchemy.engine as sa_engine
 import sqlalchemy.schema as sa_schema
+from tinytim.rows import row_dicts_to_data
+from tinytim.data import column_names
 
 import sqlalchemize.type_convert as type_convert
 import sqlalchemize.features as features
+import sqlalchemize.insert as insert
+
+Record = dict[str, Any]
 
 
 def create_table(
@@ -39,3 +46,45 @@ def create_table(
     table_creation_sql = sa_schema.CreateTable(table)
     engine.execute(table_creation_sql)
     return features.get_table(table_name, engine, schema=schema)
+
+
+def create_table_from_records(
+    table_name: str,
+    records: Sequence[Record],
+    primary_key: str,
+    engine: sa_engine.Engine,
+    column_types: Optional[Sequence] = None,
+    schema: Optional[str] = None,
+    autoincrement: Optional[bool] = True,
+    if_exists: Optional[str] = 'error',
+    columns: Optional[Sequence[str]] = None,
+    missing_value: Optional[Any] = None
+) -> sa.Table:
+    data = row_dicts_to_data(records, columns, missing_value)
+    if column_types is None:
+        column_types = [column_datatype(values) for values in data.values()]
+    col_names = column_names(data)
+    table = create_table(table_name, col_names, column_types, primary_key, engine, schema, autoincrement, if_exists)
+    insert.insert_records(table, records, engine)
+    return table
+
+
+def column_datatype(values: Iterable) -> type:
+    dtypes = [
+        int, str, int | float, decimal.Decimal, datetime.datetime,
+        bytes, bool, datetime.date, datetime.time, 
+        datetime.timedelta, list, dict
+    ]
+    for value in values:
+        for dtype in list(dtypes):
+            if not isinstance(value, dtype):
+                dtypes.pop(dtypes.index(dtype))
+    if len(dtypes) == 2:
+        if set([int, float | int]) == {int, float | int}:
+            return int
+    if len(dtypes) == 1:
+        if dtypes[0] == float | int:
+            return float
+        return dtypes[0]
+    return str
+    
