4d040cbf2244910cfac6e1bd52d280a6b8991500
Odos Matthews <odosmatthews@Odoss-Mac-mini.local>
2023-02-02 16:13:55 -0500

    add features tests

--- tests/test_features.py
+++ tests/test_features.py
@@ -1,13 +1,79 @@
 import unittest
 
-from sqlalchemize.features import primary_key_columns
+from sqlalchemy import INTEGER, MetaData, Table
+from sqlalchemy.orm.decl_api import DeclarativeMeta
+from sqlalchemy.engine.base import Connection
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm.session import Session
+
+from sqlalchemize.features import get_class, get_table, primary_key_columns, primary_key_names
+from sqlalchemize.features import get_connection, get_session, get_metadata
+from sqlalchemize.features import get_engine_table
 from tests.setup_db import create_table
 
 
 connection_string = 'sqlite://'
 
 
-class TestPrimaryKeyColumns(unittest.TestCase):
+class TestPrimaryKeys(unittest.TestCase):
     def test_primary_key_columns(self):
         engine, table = create_table(connection_string)
+        results = primary_key_columns(table)
+        names = [c.name for c in results]
+        types = [type(c.type) for c in results]
+        self.assertListEqual(names, ['id'])
+        self.assertListEqual(types, [INTEGER])
+
+    def test_primary_key_names(self):
+        engine, table = create_table(connection_string)
+        results = primary_key_names(table)
+        expected = ['id']
+        self.assertListEqual(results, expected)
+
+
+class TestConnection(unittest.TestCase):
+    def test_get_connection(self):
+        engine, table = create_table(connection_string)
+        session = get_session(engine)
+        con = get_connection(session)
+        results = type(con)
+        expected = Connection
+        self.assertEqual(results, expected)
+
+    def test_get_session(self):
+        engine, table = create_table(connection_string)
+        session = get_session(engine)
+        result = type(session)
+        expected = Session
+        self.assertEqual(result, expected)
+
+class TestMetaData(unittest.TestCase):
+    def test_get_metadata(self):
+        engine, table = create_table(connection_string)
+        meta = get_metadata(engine)
+        results = type(meta)
+        expected = MetaData
+        self.assertEqual(results, expected)
+    
+    def test_get_table(self):
+        engine, table = create_table(connection_string)
+        result_table = get_table('xy', engine)
+        results = result_table.name, result_table.bind, type(result_table)
+        expected = 'xy', engine, Table
+        self.assertEqual(results, expected)
+
+    def test_get_engine_table(self):
+        con_str = 'sqlite:///data/test.db'
+        create_table(con_str)
+        results = get_engine_table(con_str, 'xy')
+        results = tuple(type(x) for x in results)
+        expected = Engine, Table
+        self.assertEqual(results, expected)
+
+    def test_get_class(self):
+        engine, table = create_table(connection_string)
+        result = get_class('xy', engine)
+        results = type(result)
+        expected = DeclarativeMeta
+        self.assertEqual(results, expected)
         
